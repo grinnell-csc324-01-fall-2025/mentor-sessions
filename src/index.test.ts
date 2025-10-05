0a601882eb7c814d1cb5b77840a4202a1d6b8a45
Tim Yu <yuyu.tim@gmail.com>
2025-10-04 21:41:59 -0500

    update for ci/cd mentor session

---
+++ src/index.test.ts
@@ -0,0 +1,43 @@
+import { expect, test, describe } from "vitest";
+import { fizzBuzz } from "./index";
+
+describe("fizzBuzz tests", () => {
+  const MAX = 100;
+  const fb = fizzBuzz(MAX);
+
+  test("outputs the correct number of elements", () => {
+    expect(fb.length).toEqual(MAX);
+  })
+
+  test("replaces any number divisible by 3 and 5 with fizzbuzz", () => {
+    const result: boolean = fb
+      .filter((_, i) => i % 3 === 0 && i % 5 === 0)
+      .map(v => v == "fizzbuzz")
+      .reduce((prev, cur) => prev && cur, true);
+    expect(result).toEqual(true);
+  });
+
+  test("replaces any number ONLY divisible by 3 and not 5 with fizz", () => {
+      const result: boolean = fb
+        .filter((_, i) => i % 3 === 0 && i % 5 !== 0)
+        .map(v => v == "fizz")
+        .reduce((prev, cur) => prev && cur, true);
+      expect(result).toEqual(true);
+  });
+
+  test("replaces any number ONLY divisible by 5 and not 3 with buzz", () => {
+      const result: boolean = fb
+        .filter((_, i) => i % 5 === 0 && i % 3 !== 0)
+        .map(v => v == "buzz")
+        .reduce((prev, cur) => prev && cur, true);
+      expect(result).toEqual(true);
+  });
+
+  test("keeps any number divisible by NEITHER 3 nor 5", () => {
+      const result: boolean = fb
+        .map((v, i) => (i % 3 !== 0 && i % 5 !== 0) ? v === i : true)
+        .reduce((prev, cur) => prev && cur, true);
+      expect(result).toEqual(true);
+  });
+
+});
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+/**
+ * Fizz Buzz implementation.
+ * @param upperBound Length of the returned array.
+ * @returns An array from 0 (inclusive) to upperBound (exclusive), but replacing
+ * any integer divisible by three with the word "fizz", and any number divisible
+ * by five with the word "buzz", and any number divisible by both three and five
+ * with the word "fizzbuzz". (Copied from Wikipedia)
+ */
+export function fizzBuzz(upperBound: number): Array<number | string> {
+  const arr: Array<number | string> = [];
+  for (let i = 0; i < upperBound; i++) {
+    // This is incorrect on purpose so I can demonstrate CI and TDD via GitHub
+    // Actions for a class.
+    if (i % 3 === 0) {
+      arr.push("fizz");
+    } else if (i % 5 === 0) {
+      arr.push("buzz");
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("fizzbuzz");
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+function main() {
+  for (const v of fizzBuzz(21)) {
+    console.log(v);
+  }
+}
+
+main();
